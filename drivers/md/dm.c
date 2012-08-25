a60c94cbc0b654a32daa9d0e3657535f962f32c1
coolexe <kimithemes@gmail.com>
2012-08-25 23:17:24 +0530

    Linaro GCC Fix
    Conflicts:
    	arch/arm/mach-msm/include/mach/restart.h
    	drivers/md/dm-table.c
    	drivers/md/dm.c
    	drivers/net/wireless/bcm4329_248/wl_iw.c
    	net/netfilter/xt_qtaguid.c

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1073,7 +1073,11 @@ static struct bio *split_bvec(struct bio *bio, sector_t sector,
 	clone->bi_flags |= 1 << BIO_CLONED;
 
 	if (bio_integrity(bio)) {
+<<<<<<< HEAD
 		rc = bio_integrity_clone(clone, bio, GFP_NOIO, bs);
+=======
+		(void) bio_integrity_clone(clone, bio, GFP_NOIO, bs);
+>>>>>>> 601f0f1... Linaro GCC Fix
 		bio_integrity_trim(clone,
 				   bio_sector_offset(bio, idx, offset), len);
 	}
@@ -1105,7 +1109,11 @@ static struct bio *clone_bio(struct bio *bio, sector_t sector,
 	clone->bi_flags &= ~(1 << BIO_SEG_VALID);
 
 	if (bio_integrity(bio)) {
+<<<<<<< HEAD
 		rc = bio_integrity_clone(clone, bio, GFP_NOIO, bs);
+=======
+		(void) bio_integrity_clone(clone, bio, GFP_NOIO, bs);
+>>>>>>> 601f0f1... Linaro GCC Fix
 
 		if (idx != bio->bi_idx || clone->bi_size < bio->bi_size)
 			bio_integrity_trim(clone,
