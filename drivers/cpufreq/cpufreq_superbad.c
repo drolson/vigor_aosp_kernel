c81617a4970de4f1adde6aae7e2f2c3e0ff9e01a
iHateWEBos <mannyb1877@gmail.com>
2013-02-05 18:50:24 -0500

    didnt like virtuous

--- drivers/cpufreq/cpufreq_superbad.c
+++ drivers/cpufreq/cpufreq_superbad.c
@@ -100,7 +100,7 @@ static unsigned long down_rate_us;
  * When ramping up frequency with no idle cycles jump to at least this frequency.
  * Zero disables. Set a very high value to jump to policy max freqeuncy.
  */
-#define DEFAULT_UP_MIN_FREQ 1516800;
+#define DEFAULT_UP_MIN_FREQ 1512000;
 static unsigned int up_min_freq;
 
 /*
@@ -109,17 +109,17 @@ static unsigned int up_min_freq;
  * to minimize wakeup issues.
  * Set sleep_max_freq=0 to disable this behavior.
  */
-#define DEFAULT_SLEEP_MAX_FREQ 230400;
+#define DEFAULT_SLEEP_MAX_FREQ 384000;
 static unsigned int sleep_max_freq;
 
 /*
  * The frequency to set when waking up from sleep.
  * When sleep_max_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 902400;
+#define DEFAULT_SLEEP_WAKEUP_FREQ 1134000;
 static unsigned int sleep_wakeup_freq;
 
-#define UP_THRESHOLD_FREQ 1516800;
+#define UP_THRESHOLD_FREQ 1512000;
 static unsigned int threshold_freq;
 
 /*
@@ -127,10 +127,10 @@ static unsigned int threshold_freq;
  * go below this frequency.
  * Set awake_min_freq=0 to disable this behavior.
  */
-#define DEFAULT_AWAKE_MIN_FREQ 368640;
+#define DEFAULT_AWAKE_MIN_FREQ 384000;
 static unsigned int awake_min_freq;
 
-static unsigned int suspendfreq = 400000;
+static unsigned int suspendfreq = 384000;
 
 /*
  * Sampling rate, I highly recommend to leave it at 2.
