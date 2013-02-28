50c4219ea115ab379be845f942c578eaae6ea58c
iHateWEBos <mannyb1877@gmail.com>
2013-02-28 14:02:53 -0500

    tweaked scary governor

--- drivers/cpufreq/cpufreq_scary.c
+++ drivers/cpufreq/cpufreq_scary.c
@@ -28,10 +28,10 @@
 
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
 #define DEF_FREQUENCY_DOWN_THRESHOLD		(45)
-#define DEFAULT_SLEEP_MAX_FREQ 1134000
+#define DEFAULT_SLEEP_MAX_FREQ 486000
 #define DEFAULT_SLEEP_MIN_FREQ 192000
-#define DEFAULT_SLEEP_PREV_FREQ 1134000 //This is so that if there are any issues resulting in sleep_prev_freq getting set, there will be a backup freq
-#define DEFAULT_PREV_MAX 1134000
+#define DEFAULT_SLEEP_PREV_FREQ 192000 //This is so that if there are any issues resulting in sleep_prev_freq getting set, there will be a backup freq
+#define DEFAULT_PREV_MAX 1836000
 static unsigned int suspended;
 static unsigned int sleep_max_freq=DEFAULT_SLEEP_MAX_FREQ;
 static unsigned int sleep_min_freq=DEFAULT_SLEEP_MIN_FREQ;
